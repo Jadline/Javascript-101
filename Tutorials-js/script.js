fc2faf6c3678ca2a76c369b84add8b35aafcee5d
Jadline <124354830+Jadline@users.noreply.github.com>
2024-06-26 07:23:55 +0000

    update changes

--- Tutorials-js/script.js
+++ Tutorials-js/script.js
@@ -22,4 +22,74 @@
 //     return num1 * 2;
 // }
 // console.log(double(3))
+// setTimeout(() => {
+//     console.log('Hello')
+//     setTimeout(() => {
+//         console.log('hey')
+//         setTimeout(()=>{
+//             console.log('namaste')
+//             setTimeout(() => {
+//                 console.log('Hi')
+//                 setTimeout(()=>{
+//                     console.log('Bonjour')
+//                 },2000)
+//             },2000)
+//         },2000)
+//     },2000)
+// },2000)
 
+//Enhanced Object Literalls 
+
+//=set of enhancements to the syntax for defining objects in js 
+// function  user (name,age,work){
+//     return {
+//         name :  name,
+//         age : age ,
+//         work : work
+//     }
+// }
+// const jad = user('jad',20,'programmer')
+// const joy = user('joy',19,'architect')
+// console.log(jad)
+// console.log(joy)
+
+// function refactorVars(num){
+//         return {
+//             num
+//         }
+// }
+// var a = refactorVars(1)
+// var b = refactorVars(2)
+// var c = refactorVars(3)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// const a = 1
+// const b = 2
+// const c = 3
+
+// const obj = {a,b,c};
+// console.log(obj)
+// function countToFive(count=false){
+//     if (count === true){
+//         for (let i =1 ; i <=5 ; i++){
+//             console.log(`count ${i}`)
+//         }
+//     }
+// }
+// countToFive()
+// function rating(rate){
+//     if (rate === 5){
+//         console.log('High rating')
+//     }
+//     else if (rate === 0){
+//         console.log('low rating')
+//     }
+// function Multiply(a,b=1){
+//    return a * b
+// }
+// console.log(Multiply(2,3))
+
+
+//
