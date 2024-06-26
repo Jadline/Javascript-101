2b9a35f1d1f1cf0afbcb07bcc8291abce833608e
Jadline <124354830+Jadline@users.noreply.github.com>
2024-06-26 06:45:32 +0000

    update changes

---
+++ Tutorials-js/script.js
@@ -0,0 +1,25 @@
+//template strings in js -allows you to create strings with embedded expression .Denoted by backticks
+// const firstName = 'Jad'
+// const lastName = 'webdev'
+// console.log(`My name is ${firstName} ${lastName}`)
+
+// //challenge
+// function greet(username){
+//     return `hello ${username}`
+// }
+// console.log(greet('jad'))
+
+// let greet = (username) =>{
+//     return `hello ${username}`
+// }
+// console.log(greet('jad'))
+// function double(num1){
+//     return num1 * 2;
+// }
+// console.log(double(2))
+
+// let double = (num1) => {
+//     return num1 * 2;
+// }
+// console.log(double(3))
+
