92981923437356c9558259018a9e0faf1828da9e
Jadline <124354830+Jadline@users.noreply.github.com>
2024-06-26 05:57:43 +0000

    updating changes

---
+++ Tutorials-js/dateTimejs.js
@@ -0,0 +1,15 @@
+const date = new Date()
+// const Year = date.getFullYear()
+// console.log('Year :',Year)
+// const month = date.getMonth()
+// console.log('month:',month)
+// const  day = date.getDay()
+// console.log('day:',day)
+// const hours = date.getHours()
+// console.log('hours:',hours)
+date.setDate(date.getDate() + 1)
+console.log(date)
+
+console.log(date.toDateString()) //converts to human readablle form wed jun 26 2024
+console.log(date.toISOString())
+console.log(date.toLocaleString())
